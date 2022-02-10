b97e6d4dac9edfa06114af54a7132a757860d690
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-02-10 09:10:12 +0900

    Group Tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,24 +1,37 @@
 import { test, expect } from '@playwright/test';
 
-test('Make Test Using Codegen', async ({ page, browserName }) => {
-  // skip(condition: boolean, description?: string)
-  test.skip(browserName === 'firefox', 'Still working on it');
+test.describe('Make Test Using Codegen', () => {
+  test('Navigation Test', async ({ page }) => {
+    // Go to https://syncross.vercel.app/
+    await page.goto('https://syncross.vercel.app/');
 
-  // Go to https://syncross.vercel.app/
-  await page.goto('https://syncross.vercel.app/');
+    // Click text=About
+    await page.click('text=About');
+    await expect(page).toHaveURL('https://syncross.vercel.app/About');
 
-  // Click text=About
-  await page.click('text=About');
-  await expect(page).toHaveURL('https://syncross.vercel.app/About');
+    // Click text=My Works
+    await page.click('text=My Works');
+    await expect(page).toHaveURL('https://syncross.vercel.app/Work');
 
-  // Click text=My Works
-  await page.click('text=My Works');
-  await expect(page).toHaveURL('https://syncross.vercel.app/Work');
+    // Click text=Home
+    await page.click('text=Home');
+    await expect(page).toHaveURL('https://syncross.vercel.app/');
 
-  // Click text=Home
-  await page.click('text=Home');
-  await expect(page).toHaveURL('https://syncross.vercel.app/');
+    // Close page
+    await page.close();
+  });
 
-  // Close page
-  await page.close();
+  test('My Works Button Test', async ({ page }) => {
+    // Go to https://syncross.vercel.app/
+    await page.goto('https://syncross.vercel.app/');
+
+    // Click text=Frontend DeveloperSynCROSS, The Knowledge ExplorerMy Works >> path
+    await page.click(
+      'text=Frontend DeveloperSynCROSS, The Knowledge ExplorerMy Works >> path',
+    );
+    await expect(page).toHaveURL('https://syncross.vercel.app/Work');
+
+    // Close page
+    await page.close();
+  });
 });

2ccac3c6615ff97569d3588d4833a26d4398ec87
SynCROSS <51585533+SynCROSS@users.noreply.github.com>
2022-02-12 13:35:00 +0900

    Tag Tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,37 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test.describe('Make Test Using Codegen', () => {
-  test('Navigation Test', async ({ page }) => {
-    // Go to https://syncross.vercel.app/
-    await page.goto('https://syncross.vercel.app/');
+test('Navigation Test @nav-test', async ({ page }) => {
+  // Go to https://syncross.vercel.app/
+  await page.goto('https://syncross.vercel.app/');
 
-    // Click text=About
-    await page.click('text=About');
-    await expect(page).toHaveURL('https://syncross.vercel.app/About');
+  // Click text=About
+  await page.click('text=About');
+  await expect(page).toHaveURL('https://syncross.vercel.app/About');
 
-    // Click text=My Works
-    await page.click('text=My Works');
-    await expect(page).toHaveURL('https://syncross.vercel.app/Work');
+  // Click text=My Works
+  await page.click('text=My Works');
+  await expect(page).toHaveURL('https://syncross.vercel.app/Work');
 
-    // Click text=Home
-    await page.click('text=Home');
-    await expect(page).toHaveURL('https://syncross.vercel.app/');
+  // Click text=Home
+  await page.click('text=Home');
+  await expect(page).toHaveURL('https://syncross.vercel.app/');
 
-    // Close page
-    await page.close();
-  });
+  // Close page
+  await page.close();
+});
 
-  test('My Works Button Test', async ({ page }) => {
-    // Go to https://syncross.vercel.app/
-    await page.goto('https://syncross.vercel.app/');
+test('My Works Button Test @my-works-btn-test', async ({ page }) => {
+  // Go to https://syncross.vercel.app/
+  await page.goto('https://syncross.vercel.app/');
 
-    // Click text=Frontend DeveloperSynCROSS, The Knowledge ExplorerMy Works >> path
-    await page.click(
-      'text=Frontend DeveloperSynCROSS, The Knowledge ExplorerMy Works >> path',
-    );
-    await expect(page).toHaveURL('https://syncross.vercel.app/Work');
+  // Click text=Frontend DeveloperSynCROSS, The Knowledge ExplorerMy Works >> path
+  await page.click(
+    'text=Frontend DeveloperSynCROSS, The Knowledge ExplorerMy Works >> path',
+  );
+  await expect(page).toHaveURL('https://syncross.vercel.app/Work');
 
-    // Close page
-    await page.close();
-  });
+  // Close page
+  await page.close();
 });
